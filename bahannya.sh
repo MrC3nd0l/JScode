e4b510e39460fb94fcc58e766b7be58ecea71f87
_/|Arya-Kun <49186597+MrC3nd0l@users.noreply.github.com>
2019-08-11 20:03:15 +0700

    Masih dalam pengembangan yah anjenk ;v

---
+++ bahannya.sh
@@ -0,0 +1,3 @@
+z="
+";uz='a ki';ez='& ap';nz='lolc';rz='toil';Bz=' "In';Zz=' "Ok';iz='pkg ';fz='t up';Cz='stal';Nz='budj';Pz='. ;v';Dz='l ba';kz='all ';qz='et';Gz=' cuy';Hz=' :*"';bz='apt ';mz='gem ';Xz='sshh';Oz='ank.';lz='ruby';Fz='dulu';Az='echo';wz=':*"';Uz=' nih';hz='e';cz='upda';tz='ne e';az='e"';dz='te &';Mz='ahh ';Kz=' "Wa';Vz='h.. ';oz='at';jz='inst';Yz='p 3';Tz='oly ';Lz='it y';pz='figl';Jz='p 1';Rz=' "La';vz='mak ';Iz='slee';gz='grad';Ez='han ';Qz='"';Wz='mmps';Sz='gi c';sz=' "Do';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$z$Iz$Jz$z$Az$Kz$Lz$Mz$Nz$Oz$Pz$Qz$z$Iz$Jz$z$Az$Rz$Sz$Tz$Fz$Uz$Vz$Wz$Xz$Qz$z$Iz$Yz$z$Az$Zz$az$z$Iz$Jz$z$bz$cz$dz$ez$fz$gz$hz$z$iz$jz$kz$lz$z$mz$jz$kz$nz$oz$z$iz$jz$kz$pz$qz$z$iz$jz$kz$rz$qz$z$Iz$Jz$z$Az$sz$tz$uz$vz$wz"
